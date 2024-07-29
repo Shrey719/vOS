d129fc786ef1d36a50d1e5c01c1946e05064580d
theskid22 <shrey150436@gmail.com>
2024-07-28 20:27:40 -0400

    Hello world v2!!! (made multi char print)

--- src/drivers/display.h
+++ src/drivers/display.h
@@ -4,3 +4,20 @@ void print_char(char c) {
 *(char*)(0xb8000+ buf_amnt)= c;
 buf_amnt = buf_amnt + 2;
 }
+
+unsigned int strlen(char* str)
+{
+    unsigned int length = 0;
+    while (*str != 0)
+    {
+        str++;
+        length++;
+    }
+    return length;
+}
+
+void print(char* str) {
+for (int i = 0; i < strlen(str); i++) {
+    print_char(str[i]);
+}
+}
--- src/kernel.c
+++ src/kernel.c
@@ -2,16 +2,6 @@
 
 
 void main(){
-    print_char('h');
-    print_char('e');
-    print_char('l');
-    print_char('l');
-    print_char('o');
-    print_char(' ');
-    print_char('w');
-    print_char('o');
-    print_char('r');
-    print_char('l');
-    print_char('d');
+    print("Hello world!");
     return;
 }
