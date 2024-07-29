73105bb93818be987d5ed427ec5edd7636c6313c
theskid22 <shrey150436@gmail.com>
2024-07-28 21:10:33 -0400

    big   ass    enum

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ i386-elf-gcc -ffreestanding -m32 -g -c "src/kernel.c" -o "Binaries/kernel.o"
 nasm "src/zeroes.asm" -f bin -o "Binaries/zeroes.bin"
 i386-elf-ld -o "Binaries/full_kernel.bin" -Ttext 0x1000 "Binaries/kernel_entry.o" "Binaries/kernel.o"  --oformat binary
 
-cat "Binaries/boot.bin" "Binaries/full_kernel.bin" "Binaries/zeroes.bin"  > "Binaries/OS.bin"
+cat "Binaries/boot.bin" "Binaries/full_kernel.bin" "Binaries/zeroes.bin"  > "Binaries/vOS.bin"
 
-qemu-system-x86_64 -drive format=raw,file="Binaries/OS.bin",index=0,if=floppy,  -m 128M
+qemu-system-x86_64 -drive format=raw,file="Binaries/vOS.bin",index=0,if=floppy,  -m 128M
